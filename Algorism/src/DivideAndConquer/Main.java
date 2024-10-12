06adc9f7cd3aa7fe195b603f1a2c63cf0efb1d7e
seho1278 <seho1278@naver.com>
2024-10-13 08:27:48 +0900

    241013

--- Algorism/src/DivideAndConquer/Main.java
+++ Algorism/src/DivideAndConquer/Main.java
@@ -1,6 +1,29 @@
 package DivideAndConquer;
 
+import java.util.Scanner;
 
+/**
+ * 백준 실버2 2630 색종이 만들기
+ * 주어진 종이를 일정한 규칙에 따라 잘라서 다양한 크기를 가진 정사각형 모양의
+ * 하얀색 또는 파란색 색종이 만들기
+ *
+ * 입력 : 첫 줄에 전체 종이 한변의 길이 N ( 2 제곱수)
+ * 둘째 줄부터 마지막 줄까지 색종이 가로줄의 정사각형 칸들의 색 (흰색0, 파란색1)
+ * 숫자 사이 빈칸 존재
+ * 출력 : 첫줄에 잘라진 하얀색 색종이 개수 출력, 둘째 줄에 파란색 색종이 개수 출력
+ */
 
 public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+    }
+
+
+
 }
